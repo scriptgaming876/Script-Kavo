7eeb91fe4f0c69a8ab27b32689d849af887d0ff3
scriptgaming876 <discordscriptgaminghelp@gmail.com>
2025-04-27 17:57:34 +0800

    Create Example.lua

---
+++ Doors/Achievement/CUSTOM ACHIEVEMENT/Example.lua
@@ -0,0 +1,9 @@
+local achievementGiver = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Custom%20Achievements/Source.lua"))()
+
+---====== Display achievement ======---
+achievementGiver({
+    Title = "Title",
+    Desc = "Description",
+    Reason = "Reason",
+    Image = "rbxassetid://12309073114"
+})
