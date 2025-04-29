9bf89e5e1309fa10a23fdd87309bcc322bf96202
scriptgaming876 <discordscriptgaminghelp@gmail.com>
2025-04-29 09:53:45 +0800

    Create Sturggle Achievement.lua

---
+++ Doors/Achievement/CUSTOM ACHIEVEMENT/Sturggle Achievement.lua
@@ -0,0 +1,9 @@
+local achievementGiver = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Custom%20Achievements/Source.lua"))()
+
+---====== Display achievement ======---
+achievementGiver({
+    Title = "Sturggle",
+    Desc = "Sturggle Fragmented V4",
+    Reason = "Ecounter Sturggle",
+    Image = "rbxassetid://133223796060477"
+})
