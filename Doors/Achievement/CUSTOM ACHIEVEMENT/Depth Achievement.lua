b6c00dae0c6d9b03815cf164a19a5ebbdfbb1785
scriptgaming876 <discordscriptgaminghelp@gmail.com>
2025-04-29 09:49:36 +0800

    Create Depth Achievement.lua

---
+++ Doors/Achievement/CUSTOM ACHIEVEMENT/Depth Achievement.lua
@@ -0,0 +1,9 @@
+local achievementGiver = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Custom%20Achievements/Source.lua"))()
+
+---====== Display achievement ======---
+achievementGiver({
+    Title = "Depth Finally The End Rebounds",
+    Desc = "Depth The End For Mahyem",
+    Reason = "Ecounter Depth",
+    Image = "rbxassetid://12545106517"
+})
