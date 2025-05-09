9afb52e5a94526b573d9fc52075d4a76bed9b1b7
scriptgaming876 <discordscriptgaminghelp@gmail.com>
2025-05-09 17:19:51 +0800

    Create Surge Achievement.lua

---
+++ Doors/Custom Achievement/Surge Achievement.lua
@@ -0,0 +1,9 @@
+local achievementGiver = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Custom%20Achievements/Source.lua"))()
+
+---====== Display achievement ======---
+achievementGiver({
+    Title = "Hide In Time",
+    Desc = "You better find a hiding spot quickly!",
+    Reason = "Survive Surge",
+    Image = "rbxassetid://136489826653027"
+})
